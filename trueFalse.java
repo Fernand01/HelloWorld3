daa5ba66e5ab9f0d2bd9c5e64e72f7af7fc07421
Jon Connor <jonconnor@Jons-MacBook-Pro.local>
2017-07-21 17:56:44 -0400

    anymessage

---
+++ trueFalse.java
@@ -0,0 +1,9 @@
+public class trueFalse {
+    public static void main(String [] args) {
+        boolean trueFalse = false;
+        if (trueFalse)
+    System.out.println("value is True");
+    else
+        System.out.println("value is False");
+    }
+}
